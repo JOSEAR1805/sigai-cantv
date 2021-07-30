1d0363adc12cf06a9a0f9c7e3ce4682a8ac6c3c1
Jose Artigas <jgam310@gmail.com>
2021-07-29 23:05:21 -0400

    commit initial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import LayoutApp from "@components/layout";
+
+const Home = () => {
+    return (
+      <LayoutApp>
+          <h1>Hola Home</h1>
+      </LayoutApp>
+    );
+}
+
+export default Home;
